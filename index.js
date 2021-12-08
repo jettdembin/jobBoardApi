1b3b5a4508c8997af3136616b0cd0748b475c784
Jett Dembin <71148367+jettdembin@users.noreply.github.com>
2021-12-08 17:14:03 -0500

    removed comments and changed url to .com

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/*
-    1. Send an AJAX request to https://remoteok.io/api
-    2. Test that request to make sure it worked
-    3. Use the data to create HTML
-*/
-
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -36,50 +30,9 @@ xhttp.onreadystatechange = function() {
                 </div>
             </a>`;
            container.appendChild(rowDiv);
-           /*rowDiv.addEventListener('mouseenter', e => {
-            rowDiv.innerHTML = `
-                <div class="left-section">
-                    <h3 class="company">${row.company}</h5>
-                    <h2 class="position">${row.position}</h3>
-                    <p class="location">${row.location}</p>
-                </div>
-                <div class="mid-section">
-                    ${row.tags.map(function(tag) {
-                        return `<h3 class="tag">${tag}</h3>`
-                    }).join('')}
-                </div>
-                <div class="mid-right-section">
-                    ${new Date(row.date).toLocaleString()}
-                </div>
-                <div class="right-section">
-                    <a href=${row.url}>
-                        <button class="apply">Apply</button>
-                    </a>
-                </div>`;
-           })
-           rowDiv.addEventListener('mouseleave', e => {
-            rowDiv.innerHTML = `
-            <div class="left-section">
-                    <h3 class="company">${row.company}</h5>
-                    <h2 class="position">${row.position}</h3>
-                    <p class="location">${row.location}</p>
-                </div>
-                <div class="mid-section">
-                    ${row.tags.map(function(tag) {
-                        return `<h3 class="tag">${tag}</h3>`
-                    }).join('')}
-                </div>
-                <div class="mid-right-section">
-                    ${new Date(row.date).toLocaleString()}
-                </div>
-                <div class="right-section">
-                </div>`;
-            
-            })
-        */
        })
     }
 };
 
-xhttp.open("GET", "https://remoteok.io/api", true);
+xhttp.open("GET", "https://remoteok.com/api", true);
 xhttp.send();
